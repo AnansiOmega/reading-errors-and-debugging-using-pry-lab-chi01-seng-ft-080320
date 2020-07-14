7c98b25e6b98cea79c04a7b02da56336c879ab8f
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-14 22:47:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -13,3 +13,5 @@ def engage
   puts state_log(date)
   date = generate_star_date
 end
+
+binding.pry
